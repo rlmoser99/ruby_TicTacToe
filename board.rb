58cfda5036ea421d4eabf1941d4ae0f06c8f96ba
Rachel Moser <moser.rachel@gmail.com>
2020-04-02 13:26:05 -0500

    format corrections

--- board.rb
+++ board.rb
@@ -1,21 +1,21 @@
 class Board
-    attr_accessor :cells
-    def initialize
-        @cells = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    end
+  attr_accessor :cells
+  def initialize
+    @cells = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  end
 
-    def show
-        puts ""
-        puts " #{cells[0]} | #{cells[1]} | #{cells[2]}"
-        puts "---+---+---"
-        puts " #{cells[3]} | #{cells[4]} | #{cells[5]}"
-        puts "---+---+---"
-        puts " #{cells[6]} | #{cells[7]} | #{cells[8]}"
-        puts ""
-    end
+  def show
+    puts ""
+    puts " #{cells[0]} | #{cells[1]} | #{cells[2]}"
+    puts "---+---+---"
+    puts " #{cells[3]} | #{cells[4]} | #{cells[5]}"
+    puts "---+---+---"
+    puts " #{cells[6]} | #{cells[7]} | #{cells[8]}"
+    puts ""
+  end
 
-    def full?(symbol_1, symbol_2)
-        @cells.all?{ | cell | cell == symbol_1 || cell == symbol_2}
-    end
+  def full?(symbol_1, symbol_2)
+    @cells.all?{ | cell | cell == symbol_1 || cell == symbol_2}
+  end
 
 end
