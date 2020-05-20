5fe34940ada376dc8a73e6de0c5c73aefe08700b
Rachel Moser <moser.rachel@gmail.com>
2020-05-20 03:00:54 -0500

    refactor game_over? and created tests

--- lib/board.rb
+++ lib/board.rb
@@ -24,4 +24,14 @@ class Board
   def full?(symbol1, symbol2)
     @cells.all? { |cell| cell == symbol1 || cell == symbol2 }
   end
+
+  def game_over?
+    winning_combinations = [
+      [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6],
+      [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]
+    ]
+    winning_combinations.any? do |combo|
+      cells[combo[0]] == cells[combo[1]] && cells[combo[0]] == cells[combo[2]]
+    end
+  end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -23,4 +23,65 @@ describe Board do
       expect(board.full?('x', 'o')).to be true
     end
   end
+
+  context '#game_over? false (continue game) & true (end game)' do
+    it 'empty board is false' do
+      board = Board.new
+      expect(board.game_over?).to be false
+    end
+
+    it 'half-played game is false' do
+      board = Board.new
+      board.cells = %w[x 2 x 4 5 o o 8 o]
+      expect(board.game_over?).to be false
+    end
+
+    it 'top 3 in a row is true' do
+      board = Board.new
+      board.cells = %w[x x x 4 5 o o 8 o]
+      expect(board.game_over?).to be true
+    end
+
+    it 'horizontal middle 3 in a row is true' do
+      board = Board.new
+      board.cells = %w[1 x 3 o o o x 8 x]
+      expect(board.game_over?).to be true
+    end
+
+    it 'bottom 3 in a row is true' do
+      board = Board.new
+      board.cells = %w[1 o o 4 o 6 x x x]
+      expect(board.game_over?).to be true
+    end
+
+    it 'left 3 in a row is true' do
+      board = Board.new
+      board.cells = %w[x 2 o x o 6 x 8 o]
+      expect(board.game_over?).to be true
+    end
+
+    it 'vertical middle 3 in a row is true' do
+      board = Board.new
+      board.cells = %w[x o 3 4 o x 7 o x]
+      expect(board.game_over?).to be true
+    end
+
+    it 'right 3 in a row is true' do
+      board = Board.new
+      board.cells = %w[1 o x 4 o x o 8 x]
+      expect(board.game_over?).to be true
+    end
+
+    it 'diagonal \ 3 in a row is true' do
+      board = Board.new
+      board.cells = %w[o 2 x 4 o x 7 x o]
+      expect(board.game_over?).to be true
+    end
+
+    it 'diagonal / 3 in a row is true' do
+      board = Board.new
+      board.cells = %w[o o x 4 x 6 x o 9]
+      expect(board.game_over?).to be true
+    end
+  end
 end
